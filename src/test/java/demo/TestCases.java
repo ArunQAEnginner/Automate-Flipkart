ad6cb4b1247009bedd756c622554a9812e791145
Arun <arunkandeepan10895@gmail.com>
2024-11-25 16:24:47 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -19,7 +22,7 @@ import java.util.logging.Level;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+    public static ChromeDriver driver;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -30,6 +33,50 @@ public class TestCases {
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
+    @Test
+    public void testCase01() throws InterruptedException{
+
+        //testCase01: Go to www.flipkart.com. Search "Washing Machine".
+        //Sort by popularity and print the count of items with rating less than or equal to 4 stars.
+        Wrappers test1 = new Wrappers();
+        test1.FlipkartHomePage();
+        test1.SearchTextBox("Washing Machine");
+        test1.ClickSearchButton();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[text()='Popularity']")));
+        test1.ClickPopularity();
+        Thread.sleep(3000);
+        int count = test1.CheckRatingLessThanOrEqualTo(4);
+        System.out.println("count of items with rating less than or equal to 4 stars : "+ count);
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException{
+        //testCase02: Search "iPhone", print the Titles and discount % of items with more than 17% discount
+        Wrappers test2 = new Wrappers();
+        test2.FlipkartHomePage();
+        Thread.sleep(4000);
+        test2.SearchTextBox("iPhone");
+        test2.ClickSearchButton();
+        test2.DiscountMoreThan(17);
+
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException{
+        //testCase03: Search "Coffee Mug", select 4 stars and above, 
+        //and print the Title and image URL of the 5 items with highest number of reviews
+
+        Wrappers test3 = new Wrappers();
+        test3.FlipkartHomePage();
+        Thread.sleep(4000);
+        test3.SearchTextBox("Coffee Mug");
+        test3.ClickSearchButton();
+        test3.SelectCustomerRating(4);
+        test3.TopHighestNoOfReviews(5);
+        
+    }
+    
     @BeforeTest
     public void startBrowser()
     {
@@ -51,6 +98,7 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,20 +3,130 @@ package demo.wrappers;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import demo.TestCases;
+
+import java.text.NumberFormat;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    ChromeDriver driver = TestCases.driver;
+    
+
+    public void FlipkartHomePage() throws InterruptedException{
+        driver.get("https://www.flipkart.com/");
+    }
+
+    public void SearchTextBox(String ProductName) throws InterruptedException{
+        WebElement SearchBox = driver.findElement(By.xpath("//input[@title='Search for Products, Brands and More']"));
+        SearchBox.sendKeys(ProductName);
+    }
+
+    public void ClickSearchButton(){
+        WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Search for Products, Brands and More']")));
+        WebElement SearchIcon = driver.findElement(By.xpath("//button[@aria-label='Search for Products, Brands and More']"));
+        SearchIcon.click();
+    }
+
+    public void ClickPopularity(){
+        WebElement Popularity = driver.findElement(By.xpath("//div[text()='Popularity']"));
+        Popularity.click();
+    }
+
+    public int CheckRatingLessThanOrEqualTo(float Rating){
+        List<WebElement> Ratings = driver.findElements(By.xpath("//div[@class='XQDdHH']"));
+        int count = 0 ;
+        for(WebElement rating : Ratings){
+           if(Float.valueOf(rating.getText()) <= Rating){
+                count++;
+           }
+        }
+
+        return count;
+    }
+
+    public void DiscountMoreThan(int Disount) throws InterruptedException{
+        List<WebElement> Products = driver.findElements(By.xpath("//div[@class='yKfJKb row']/div[2]//span"));
+        
+        for(WebElement Product: Products){
+            String[] discount = Product.getText().split("%");
+
+            if(Integer.valueOf(discount[0]) > Disount){
+                WebElement ProductName = Product.findElement(By.xpath(".//ancestor::div[@class='col col-5-12 BfVC2z']/preceding-sibling::div/div[1]"));
+                System.out.println("Title of item : "+ProductName.getText()+" Discount % of item : "+discount[0]);
+            }
+                          
+        }
+    }
+
+    public void SelectCustomerRating(int Rating) throws InterruptedException{
+        Thread.sleep(4000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        if(Rating >= 4){
+            WebElement CheckboxRating4AndAbove = driver.findElement(By.xpath("(//div[text()='Customer Ratings']/parent::div/following-sibling::div//label/div[1])[1]"));
+            //js.executeScript("arguments[0].scrollIntoView();",CheckboxRating4AndAbove);
+            Thread.sleep(4000);
+            CheckboxRating4AndAbove.click();
+            Thread.sleep(4000);
+
+        }
+        else if(Rating >= 3){
+            WebElement CheckboxRating3AndAbove = driver.findElement(By.xpath("(//div[text()='Customer Ratings']/parent::div/following-sibling::div//label/div[1])[2]"));
+            CheckboxRating3AndAbove.click();
+        }
+        else if (Rating >= 2) {
+            WebElement CheckboxRating2AndAbove = driver.findElement(By.xpath("(//div[text()='Customer Ratings']/parent::div/following-sibling::div//label/div[1])[3]"));
+            CheckboxRating2AndAbove.click();
+        }
+        else if(Rating >= 1){
+            WebElement CheckboxRating1AndAbove = driver.findElement(By.xpath("(//div[text()='Customer Ratings']/parent::div/following-sibling::div//label/div[1])[4]"));
+            CheckboxRating1AndAbove.click();
+        }
+    }
+
+    public void TopHighestNoOfReviews(int NoOfProducts){
+        List<WebElement> ReviewsCountOnProducts = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(WebElement ReviewsCountOnProduct : ReviewsCountOnProducts){
+        String ReviewCount = ReviewsCountOnProduct.getText().replaceAll("[(),]", "");
+        int Reviewcount = Integer.parseInt(ReviewCount);
+        
+            set.add(Reviewcount);
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list,Collections.reverseOrder());
+
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+
+        for(int i=0;i<NoOfProducts;i++){
+            String usformat = numberFormat.format(list.get(i));
+            String ProductTitle = driver.findElement(By.xpath("(//span[text()='("+usformat+")']/parent::div/preceding-sibling::a)[2]")).getText();
+            String ImageUrl = driver.findElement(By.xpath("//span[text()='("+usformat+")']/parent::div/preceding-sibling::a//img")).getAttribute("src");
+            System.out.println("Title : "+ProductTitle+" ImageUrl : "+ImageUrl);
+        }
+    }
+
+
 }
